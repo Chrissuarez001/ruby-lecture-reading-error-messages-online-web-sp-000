e47a3d8193d0f00d910bbd410afda656365564e2
benign-callee-9855 <chrissuarez001@gmail.com>
2021-04-21 00:12:06 +0000

    Automatically backed up by Learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -4,7 +4,7 @@ describe  do
     it 'raises a NameError when encountering undefined barewords' do
       expect{
         load './lib/a_name_error.rb'
-      }.to_not raise_error
+      }.to raise_error
     end
   end
 
@@ -12,7 +12,7 @@ describe  do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
         load './lib/a_syntax_error.rb'
-      }.to_not raise_error
+      }.to raise_error
     end
   end
 
