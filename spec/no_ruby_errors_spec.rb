c7759e8360b94c641611926a5a4ad5a94d798561
benign-callee-9855 <chrissuarez001@gmail.com>
2021-04-21 00:14:17 +0000

    Automatically backed up by Learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -28,7 +28,7 @@ describe  do
     it 'raises a ZeroDivisionError for dividing by zero' do
       expect{
         load './lib/a_division_by_zero_error.rb'
-      }.to_not raise_error
+      }.to raise_error
     end
   end
 end
